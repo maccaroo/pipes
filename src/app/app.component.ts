b9b175df5a2abac3f1a51c02f52b5791388f8f3c
= <maciej.krzysik@servian.com>
2023-01-11 14:40:04 +0800

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = "";
+  date: string = "";
+  amount: number = 0;
+  miles: number = 0;
+
+  onNameChange(event: any) {
+    this.name = event.target.value;
+  }
+
+  onDateChange(event: any) {
+    this.date = event.target.value;
+  }
+
+  onAmountChange(event: any) {
+    this.amount = parseFloat(event.target.value);
+  }
+
+  onMilesChange(event: any) {
+    this.miles = parseFloat(event.target.value);
+  }
 }
